49ebfb3622109e91619084453d11a7711633b7e1
salthew <adrianawsztel@gmail.com>
2025-03-28 15:19:47 +0100

    push 2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flask-WWW-ksiazkakul
 WWW - zad classroom 11.03 - flask - ksiazka kulinarna
+# Zadanie z dnia 11.03.2025
+https://classroom.google.com/u/1/c/NzEyNTQyNjkyMjYx/a/Njg5NTgwMTY0NTE0/details
+flask, ksiazka kulinarna 
+- stworzone przez Adriana Awsztel
--- app.py
+++ app.py
@@ -7,22 +7,48 @@ recipes = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', recipes=recipes)
+    search_query = request.args.get('search', '').lower()
+    filtered_recipes = [recipe for recipe in recipes if search_query in recipe['title'].lower() or search_query in recipe['ingredients'].lower()]
+    return render_template('index.html', recipes=filtered_recipes, search_query=search_query)
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/recipe/<int:recipe_id>')
+def recipe_details(recipe_id):
+    if 0 <= recipe_id < len(recipes):
+        return render_template('recipe_details.html', recipe=recipes[recipe_id])
+    return redirect(url_for('index'))
+
+@app.route('/add_recipe', methods=['GET', 'POST'])
 def add_recipe():
     if request.method == 'POST':
-        # Pobierz dane z formularza i dodaj przepis do listy
         recipe = {
             'title': request.form['title'],
             'ingredients': request.form['ingredients'],
+            'preparation_time': request.form['preparation_time'],
             'instructions': request.form['instructions']
         }
         recipes.append(recipe)
         return redirect(url_for('index'))
     return render_template('add_recipe.html')
 
-# Tutaj można dodać więcej tras dla edycji, szczegółów i usuwania
+@app.route('/edit_recipe/<int:recipe_id>', methods=['GET', 'POST'])
+def edit_recipe(recipe_id):
+    if 0 <= recipe_id < len(recipes):
+        if request.method == 'POST':
+            recipes[recipe_id] = {
+                'title': request.form['title'],
+                'ingredients': request.form['ingredients'],
+                'preparation_time': request.form['preparation_time'],
+                'instructions': request.form['instructions']
+            }
+            return redirect(url_for('index'))
+        return render_template('edit_recipe.html', recipe=recipes[recipe_id], recipe_id=recipe_id)
+    return redirect(url_for('index'))
+
+@app.route('/delete_recipe/<int:recipe_id>', methods=['POST'])
+def delete_recipe(recipe_id):
+    if 0 <= recipe_id < len(recipes):
+        recipes.pop(recipe_id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
