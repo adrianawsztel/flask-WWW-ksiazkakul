0ebd7bc0b04c282afc458e90e7fac5204fb600b6
salthew <adrianawsztel@gmail.com>
2025-03-29 09:24:19 +0100

    push 3

--- app.py
+++ app.py
@@ -2,26 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Tymczasowa baza danych w pamięci
 recipes = []
 
 @app.route('/')
 def index():
-    search_query = request.args.get('search', '').lower()
-    filtered_recipes = [recipe for recipe in recipes if search_query in recipe['title'].lower() or search_query in recipe['ingredients'].lower()]
-    return render_template('index.html', recipes=filtered_recipes, search_query=search_query)
+    search_query = request.args.get('search', '')
+    filtered_recipes = [recipe for recipe in recipes if search_query.lower() in recipe['name'].lower()]
+    return render_template('index.html', recipes=filtered_recipes)
 
-@app.route('/recipe/<int:recipe_id>')
-def recipe_details(recipe_id):
-    if 0 <= recipe_id < len(recipes):
-        return render_template('recipe_details.html', recipe=recipes[recipe_id])
-    return redirect(url_for('index'))
 
 @app.route('/add_recipe', methods=['GET', 'POST'])
 def add_recipe():
     if request.method == 'POST':
         recipe = {
-            'title': request.form['title'],
+            'name': request.form['name'],
             'ingredients': request.form['ingredients'],
             'preparation_time': request.form['preparation_time'],
             'instructions': request.form['instructions']
@@ -30,25 +24,29 @@ def add_recipe():
         return redirect(url_for('index'))
     return render_template('add_recipe.html')
 
-@app.route('/edit_recipe/<int:recipe_id>', methods=['GET', 'POST'])
+@app.route('/recipe')
+def recipe_details(recipe_id):
+    recipe = recipes[recipe_id]
+    return render_template('recipe_details.html', recipe=recipe, recipe_id=recipe_id)
+
+@app.route('/edit', methods=['GET', 'POST'])
 def edit_recipe(recipe_id):
-    if 0 <= recipe_id < len(recipes):
-        if request.method == 'POST':
-            recipes[recipe_id] = {
-                'title': request.form['title'],
-                'ingredients': request.form['ingredients'],
-                'preparation_time': request.form['preparation_time'],
-                'instructions': request.form['instructions']
-            }
-            return redirect(url_for('index'))
-        return render_template('edit_recipe.html', recipe=recipes[recipe_id], recipe_id=recipe_id)
+    if request.method == 'POST':
+        recipes[recipe_id] = {
+            'name': request.form['name'],
+            'ingredients': request.form['ingredients'],
+            'preparation_time': request.form['preparation_time'],
+            'instructions': request.form['instructions']
+        }
+        return redirect(url_for('index'))
+    recipe = recipes[recipe_id]
+    return render_template('edit_recipe.html', recipe=recipe, recipe_id=recipe_id)
+
+@app.route('/delete')
+def delete_recipe(recipe_id):
+    recipes.pop(recipe_id)
     return redirect(url_for('index'))
 
-@app.route('/delete_recipe/<int:recipe_id>', methods=['POST'])
-def delete_recipe(recipe_id):
-    if 0 <= recipe_id < len(recipes):
-        recipes.pop(recipe_id)
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
